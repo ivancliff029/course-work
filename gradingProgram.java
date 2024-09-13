409ee47d476dd8d0a6f781f024f44cbbddfcf7b3
ivancliff029 <odekeivancliff@gmail.com>
2024-09-13 21:55:05 +0300

    Fix : Handle non int inputs

--- gradingProgram.java
+++ gradingProgram.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 
 public class gradingProgram {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
         Scanner grader = new Scanner(System.in);
-        System.out.println("Enter marks");
-        int grade = grader.nextInt();
+        int grade = -1;
 
-        if ( grade > 90 ){
+        while (true) {
+            System.out.println("Enter marks (numbers only):");
+            if (grader.hasNextInt()) {
+                grade = grader.nextInt();
+                break;  
+            } else {
+                System.out.println("Invalid input! Please enter a number.");
+                grader.next();  
+            }
+        }
+
+       
+        if (grade > 90) {
             System.out.println("Excellent");
-        }else if( grade >= 80 ){
+        } else if (grade >= 80) {
             System.out.println("Very Good");
-        }else if( grade >= 70){
+        } else if (grade >= 70) {
             System.out.println("Good");
-        }else if( grade >= 60){
+        } else if (grade >= 60) {
             System.out.println("Medium");
-        }else if( grade >= 50){
+        } else if (grade >= 50) {
             System.out.println("Pass");
-        }else{
+        } else {
             System.out.println("Fail");
         }
+
         grader.close();
     }
 }
